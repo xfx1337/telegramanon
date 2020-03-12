df8c8df97a81ab02a625a952ba2db569a36db9d3
XERSIX <54548160+XTOOL-OFF@users.noreply.github.com>
2020-03-12 17:48:27 +0500

    Add files via upload

--- bot.py
+++ bot.py
@@ -17,30 +17,40 @@ def start_message(message):
 @bot.message_handler(content_types=['text'])
 def send_text(message):
 	if message.text.lower() == 'отправить сообщение':
-		def mess(message):
-			print(message.chat.id)
-			try :
-				userm = message.text
-				print(userm)
-				print(message.chat.username)
-				print(message.chat.id)			
-				print(message.chat.first_name)
-				print(message.chat.last_name)
-				print("-------")
-			except:
+		f = open("black.txt")
+		for line in f:
+			if message.chat.id == line:
+				bot.send_message(message.chat.id, "Вы получили бан, если вы не согласны напишите в лс поддержке")
+			else:
 				pass
+		try:
+			def mess(message):
+				print(message.chat.id)
+				try :
+					userm = message.text
+					print(userm)
+					print(message.chat.username)
+					print(message.chat.id)
+					print(message.chat.first_name)
+					print(message.chat.last_name)
+					print("-------")
+				except:
+					pass
+				f = open("ids.txt", "r")
+				for line in f:
+					try:
+						bot.send_message(line, userm)
+						bot.send_message(line, message.photo)
+					except:
+						pass
 			f = open("ids.txt", "r")
 			for line in f:
 				try:
-					bot.send_message(line, userm)
+					bot.send_message(line, "Пользователь пишет")
 				except:
 					pass
-		f = open("ids.txt", "r")
-		for line in f:
-			try:
-				bot.send_message(line, "Пользователь пишет")
-			except:
-				pass
-		sent = bot.send_message(message.chat.id, "Введи сообщение")
-		bot.register_next_step_handler(sent, mess)
+			sent = bot.send_message(message.chat.id, "Введи сообщение")
+			bot.register_next_step_handler(sent, mess)
+		except:
+			pass
 bot.polling(none_stop=True)
