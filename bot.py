12b398ed56d1225b6ee5c317647d4b5aed2892d3
XERSIX <54548160+XTOOL-OFF@users.noreply.github.com>
2020-03-12 15:57:54 +0500

    Add files via upload

--- bot.py
+++ bot.py
@@ -33,7 +33,7 @@ def send_text(message):
 			for line in f:
 				try:
 					bot.send_message(line, userm)
-					bot.send_message(line, message.content)
+					bot.send_message(line, message)
 				except:
 					pass
 		f = open("ids.txt", "r")
