b2d43bce67ee8f1f690cc014b90ca6361f60d924
XERSIX <54548160+XTOOL-OFF@users.noreply.github.com>
2020-03-16 13:23:19 +0500

    Add files via upload

--- bot.py
+++ bot.py
@@ -62,15 +62,18 @@ def send_text(message):
 			pass
 	if message.text.lower() == 'курс валют':
 		rub_dol = c.get_rate('USD', "RUB")
-		rub_dol2 = str(rub_dol)
+		rub_dol3 = round(rub_dol, 2)
+		rub_dol2 = str(rub_dol3)
 		rub_dolx = "USD(Доллар) = " + rub_dol2 + "RUB(Рубль)"
 		bot.send_message(message.chat.id, rub_dolx)
 		btc_usd = b.get_latest_price('USD')
-		btc_usd2 = str(btc_usd)
+		btc_usd3 = round(btc_usd, 2)
+		btc_usd2 = str(btc_usd3)
 		btc_usdx = "BTC(Bitcoin) = " + btc_usd2 + "USD(Доллар)"
 		bot.send_message(message.chat.id, btc_usdx)
 		btc_rub = b.get_latest_price('RUB')
-		btc_rub2 = str(btc_rub)
+		btc_rub3 = round(btc_rub2, 2)
+		btc_rub2 = str(btc_rub3)
 		btc_rubx = "BTC(Bitcoin) = " + btc_rub2 + " RUB(Рубль)"
 		bot.send_message(message.chat.id, btc_rubx)
 bot.polling(none_stop=True)
