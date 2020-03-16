1c627499286d065f5c16a25a7e11508c18694f7f
XERSIX <54548160+XTOOL-OFF@users.noreply.github.com>
2020-03-16 13:27:10 +0500

    Add files via upload

--- bot.py
+++ bot.py
@@ -10,6 +10,7 @@ c = CurrencyRates()
 bot = telebot.TeleBot('964957577:AAHQlnTDLdyLxDsrnsSE8M0HcxRwMup6YDk')
 keyboard1 = telebot.types.ReplyKeyboardMarkup(True, False)
 keyboard1.row('Отправить сообщение')
+keyboard1.row('Курс валют')
 @bot.message_handler(commands=['start'])
 def start_message(message):
     msg = "Привет!"
@@ -64,15 +65,15 @@ def send_text(message):
 		rub_dol = c.get_rate('USD', "RUB")
 		rub_dol3 = round(rub_dol, 2)
 		rub_dol2 = str(rub_dol3)
-		rub_dolx = "USD(Доллар) = " + rub_dol2 + "RUB(Рубль)"
+		rub_dolx = "USD(Доллар) = " + rub_dol2 + " RUB(Рубль)"
 		bot.send_message(message.chat.id, rub_dolx)
 		btc_usd = b.get_latest_price('USD')
 		btc_usd3 = round(btc_usd, 2)
 		btc_usd2 = str(btc_usd3)
-		btc_usdx = "BTC(Bitcoin) = " + btc_usd2 + "USD(Доллар)"
+		btc_usdx = "BTC(Bitcoin) = " + btc_usd2 + " USD(Доллар)"
 		bot.send_message(message.chat.id, btc_usdx)
 		btc_rub = b.get_latest_price('RUB')
-		btc_rub3 = round(btc_rub2, 2)
+		btc_rub3 = round(btc_rub, 2)
 		btc_rub2 = str(btc_rub3)
 		btc_rubx = "BTC(Bitcoin) = " + btc_rub2 + " RUB(Рубль)"
 		bot.send_message(message.chat.id, btc_rubx)
